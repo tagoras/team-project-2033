e2a30ba74597948cbb491d9fbdb19d1e16d6c036
p_vaitkevicius <pvaitkeviciuz@gmail.com>
2021-11-29 07:59:16 +0000

    navbar component created

---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,16 @@
+import react from "react";
+import { Component } from "react";
+
+export class Navbar extends Component{
+    render(){
+        return(
+            <navbar>
+                <ul>
+                    <li><a href="#">About Us</a></li>
+                    <li><a href="#"></a>Login</li>
+                    <li><a href="#"></a>Get In Touch</li>
+                </ul>
+            </navbar>
+        )
+    }
+}
