c0c7bc39b28e465f9fec50ca322e0a35cda5cf5b
p_vaitkevicius <pvaitkeviciuz@gmail.com>
2021-11-29 09:13:27 +0000

    Added showcase styling

--- src/Components/Showcase/Showcase.js
+++ src/Components/Showcase/Showcase.js
@@ -1,11 +1,13 @@
 import react, {Component} from "react";
 import photo from '/Users/pvait/team-project-2033/src/Photos/photo7.jpg';
+import '../Showcase/Showcase.css';
 
 export class Showcase extends Component{
     render(){
         return(
-            <div>
-                <img src={photo}></img>
+            <div className="ShowcaseContainer">
+                <div className="Filter"/>
+                <h1>Let's build <br/> Infrastructure <br/> Together</h1>
             </div>
         )
     }
