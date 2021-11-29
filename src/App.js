845c45d977e3e12cb240cb7680baac2368aa4c94
p_vaitkevicius <pvaitkeviciuz@gmail.com>
2021-11-29 07:57:30 +0000

    App() now renders navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { Component } from 'react';
-import {TodoPage} from './Components/Pages/TodoPage';
+import {Navbar} from './Components/Navbar/Navbar';
+
+
 
 function App() {
   return (
-    <TodoPage/>
+    <Navbar/>
   );
 }
 
