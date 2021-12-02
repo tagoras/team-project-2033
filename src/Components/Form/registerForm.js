a99bdc9d4c37d2340579519ed46148eb9d088760
vaici <i.vaicius1@newcastle.ac.uk>
2021-12-02 21:22:26 +0000

    add css styling

--- src/Components/Form/registerForm.js
+++ src/Components/Form/registerForm.js
@@ -4,22 +4,22 @@ import "./FormModules.css";
 function registerForm(){
     return <Block>
         <form className={"form"}>
-            <div className={"legend"}>Registration Form</div>
+            <div className={"legend"}>Register</div>
             <div className={"control"}>
                 <label htmlFor="username">Username</label>
                 <input type='text' required id='username'/>
             </div>
             <div className={"control"}>
                 <label htmlFor="password">Password</label>
-                <input type='text' required id='password'/>
+                <input type='text' required id='password' placeholder='********'/>
             </div>
             <div className={"control"}>
                 <label htmlFor="email">Email</label>
-                <input type='text' required id='email'/>
+                <input type='text' required id='email' placeholder='name@email.com'/>
             </div>
             <div className={"control"}>
                 <label htmlFor="postcode">Postcode</label>
-                <input type='text' required id='postcode'/>
+                <input type='text' required id='postcode' placeholder='e.g. NE4 5TG'/>
             </div>
             <div className={"actions"}>
                 <button>Submit</button>
--- src/Pages/register.js
+++ src/Pages/register.js
@@ -1,8 +1,11 @@
 import registerForm from "../Components/Form/registerForm.js";
-
+import "../Pages/Register.css"
 function registerPage(){
     return <section>
+        <div className={"register"}>
         {registerForm()}
+        </div>
+        
     </section>;
 }
 export default registerPage;
