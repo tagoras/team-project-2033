db03b42fc6d8ca84f7883ec36ab66fedc0941bb8
vaici <i.vaicius1@newcastle.ac.uk>
2021-12-05 23:57:46 +0000

    fix component, add function for posting to the DB

--- src/Pages/register.js
+++ src/Pages/register.js
@@ -1,11 +1,18 @@
-import registerForm from "../Components/Form/registerForm.js";
+import RegisterForm from "../Components/Form/registerForm.js";
 import "../Pages/Register.css"
-function registerPage(){
+function RegisterPage(){
+    function addRegisterHandler(registerData){
+        
+      //  fetch('database url',{
+      //      method:'POST',
+      //      body: JSON.stringify(registerData)
+      //  })
+    }
     return <section>
         <div className={"register"}>
-        {registerForm()}
+        <RegisterForm addRegisterForm={addRegisterHandler}/>
         </div>
         
     </section>;
 }
-export default registerPage;
+export default RegisterPage;
