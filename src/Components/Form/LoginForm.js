2290cf7a4ce143882884b7108408ad30c6a7c426
vaici <i.vaicius1@newcastle.ac.uk>
2021-12-09 22:47:46 +0000

    add function for handling login submissions

--- src/Components/Form/LoginForm.js
+++ src/Components/Form/LoginForm.js
@@ -1,16 +1,30 @@
 import Block from "../UI/Block.js";
 import "./FormModules.css";
+import {useRef} from 'react';
 
-function loginForm(){
+function LoginForm(props){
+    const usernameInputRef = useRef();
+    const passwordInputRef = useRef();
+    function loginSubmitHandler(event){
+        event.preventDefault();
+        const enteredUsername = usernameInputRef.current.value;
+        const enteredPassword = passwordInputRef.current.value;
+        const loginData={
+            username: enteredUsername,
+            password: enteredPassword,
+        }
+        props.addLoginForm(loginData);
+
+    }
     return (
     <div className="registrationContainer">
-        <form className="form">
+        <form className="form" onSubmit={loginSubmitHandler}>
             <div className="legend">Login</div>
             <div className="control">
-                <input type='text' required id='username' placeholder='Username'/>
+                <input type='text' required id='username' placeholder='Username' ref={usernameInputRef}/>
             </div>
             <div className="control">
-                <input type='text' required id='password' placeholder='********'/>
+                <input type='text' required id='password' placeholder='********' ref={passwordInputRef}/>
             </div>
             <div className="actions">
                 <button>Log in</button>
@@ -20,4 +34,4 @@ function loginForm(){
     );
 }
 
-export default loginForm;
+export default LoginForm;
