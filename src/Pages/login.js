f8db619ff2a22e9580876a816eb425763469f6c1
vaici <i.vaicius1@newcastle.ac.uk>
2021-12-09 22:49:09 +0000

    add handler function

--- src/Pages/login.js
+++ src/Pages/login.js
@@ -1,8 +1,17 @@
 import LoginForm from "../Components/Form/LoginForm.js";
 function LoginPage (){
-    return <section>
-         <div>{<LoginForm/>}</div>
-
-</section>;
+    function addLoginHandler(loginData){
+        console.log(JSON.stringify(loginData))
+        fetch('/login',{
+            method:'GET',
+            body: JSON.stringify(loginData)
+        }).then((value) => console.log(value), () => console.log("ERROR"));
+    }
+     return <section>
+        <div className="login">
+        <LoginForm addLoginForm={addLoginHandler}/>
+        </div>
+     </section>;
+    
 }
-export default LoginPage; 
+export default LoginPage;
