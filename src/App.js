93bb7dd81e646a0775253b22d85831071830b143
p_vaitkevicius <pvaitkeviciuz@gmail.com>
2021-12-09 11:20:27 +0000

    Refactored App.js class, added conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,27 +1,40 @@
-import React, { useState, useEffect } from 'react';
+import React, {Component } from 'react';
 import './App.css';
 import {Navbar} from './Components/Navbar/Navbar';
 import {Showcase} from './Components/Showcase/Showcase';
-import {Route, Routes} from 'react-router-dom';
-import LoginPage from './Pages/login';
-import RegisterPage from './Pages/register';
-import RegisterForm from './Components/Form/RegisterForm';
+import RegisterForm from './Components/RegisterForm/RegisterForm';
+
+import LoginForm from './Components/LoginForm/LoginForm';
 
 
 
-function App() {
-  return (
-    <div className="AppContainer">
-      <Navbar/>
-      <Showcase/>
-      <RegisterForm/>
-      {/* <Routes>
-        <Route path='/home' element={<Showcase/>}/>
-        <Route path='/register' element ={<RegisterPage/>}/>
-        <Route path='/login' element={<LoginPage/>}/>
-      </Routes> */}
-    </div>
-  );
+class App extends Component {
+  
+  constructor(props){
+    super(props);
+    this.state = {
+      isRegistering: true,
+    };
+  }
+
+  render(){
+    let isLoggedIn = this.state.isRegistering;
+    console.log(isLoggedIn);
+    return (
+      <div className="AppContainer">
+        <Navbar/>
+        <Showcase/>
+        {isLoggedIn ? <RegisterForm/> : <LoginForm/>}
+        
+        {/* <Routes>
+          <Route path='/home' element={<Showcase/>}/>
+          <Route path='/register' element ={<RegisterPage/>}/>
+          <Route path='/login' element={<LoginPage/>}/>
+        </Routes> */}
+      </div>
+    );
+  }
+
 }
 
 export default App;
