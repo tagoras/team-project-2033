65094f3b796107362a4aabe3cc192166e9209bae
vaici <i.vaicius1@newcastle.ac.uk>
2021-12-10 00:10:19 +0000

    add a function to redirect user after submiting

--- src/Pages/login.js
+++ src/Pages/login.js
@@ -1,11 +1,20 @@
 import LoginForm from "../Components/Form/LoginForm.js";
+import {useNavigate} from "react-router-dom";
 function LoginPage (){
+    const navigate = useNavigate();
     function addLoginHandler(loginData){
         console.log(JSON.stringify(loginData))
         fetch('/login',{
             method:'GET',
             body: JSON.stringify(loginData)
-        }).then((value) => console.log(value), () => console.log("ERROR"));
+        }).then((value) => {
+            console.log(value);
+           /*
+           TODO: add a link from login page to someother page 
+            navigate('/somepage');
+           */
+        },
+         () => console.log("ERROR"));
     }
      return <section>
         <div className="login">
--- src/Pages/register.js
+++ src/Pages/register.js
@@ -1,12 +1,18 @@
 import RegisterForm from "../Components/Form/registerForm.js";
-import "../Pages/Register.css"
+import "../Pages/Register.css";
+import {useNavigate} from "react-router-dom";
 function RegisterPage(){
+ const navigate = useNavigate();
     function addRegisterHandler(registerData){
         console.log(JSON.stringify(registerData))
         fetch('/register2',{
             method:'POST',
             body: JSON.stringify(registerData)
-        }).then((value) => console.log(value), () => console.log("ERROR"));
+        }).then((value) => {
+            console.log(value);
+            navigate('/login');
+        },
+         () => console.log("ERROR"));
     }
     return <section>
         <div className="register">
