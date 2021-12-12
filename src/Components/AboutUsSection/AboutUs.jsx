54a0574fe40525e7d0d16330676e4ea68b7fc329
p_vaitkevicius <pvaitkeviciuz@gmail.com>
2021-12-12 18:42:45 +0000

    Added original text

--- src/Components/AboutUsSection/AboutUs.jsx
+++ src/Components/AboutUsSection/AboutUs.jsx
@@ -7,15 +7,14 @@ export const AboutUs = () => {
       <div className="contentHolder">
         <h1>About Us</h1>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce
-          pulvinar ex odio, id dapibus augue consectetur at. In erat nibh,
-          dignissim vitae elementum quis, blandit id nunc. Suspendisse
-          porttitor, leo vel pulvinar mollis, justo leo luctus mauris, sed
-          dapibus arcu ligula egestas urna. Quisque consequat imperdiet nulla,
-          non tempus ex dapibus sit amet. Aenean iaculis nisi sed nulla sodales,
-          eu feugiat odio consequat. Pellentesque vehicula dui sed sem eleifend,
-          eget.
+          <span className="organizationName">[NAME]</span> is an organization that helps The Government of the United
+          Kingdom to better tackle infrastructure issues by allowing direct
+          communication with it's citizens.
         </p>
+        <p>
+          Here you can submit infrastructure complaints that you would like to be addressed at a local level.
+        </p>
+        <a className="Button-1" href="/home">Get Started</a>
       </div>
     </div>
   );
