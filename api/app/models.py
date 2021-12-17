16885759adde15085d3ede24ebc656f621b712e7
c0025335 <c0025335@newcastle.ac.uk>
2021-12-17 20:14:50 +0000

    Adjusted password length, so it can contain the full length of the hash password

--- api/app/models.py
+++ api/app/models.py
@@ -8,7 +8,7 @@ class User(db.Model, UserMixin):
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), nullable=False, unique=True)
-    password = db.Column(db.String(100), nullable=False)
+    password = db.Column(db.String(200), nullable=False)
     email = db.Column(db.String(100), nullable=False)
     postcode = db.Column(db.String(100), nullable=False)
 
