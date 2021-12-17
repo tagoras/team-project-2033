47ad9d17fca0bfaaf86929013727cdf1135730ad
c0025335 <c0025335@newcastle.ac.uk>
2021-12-17 20:14:02 +0000

    Added login route

--- api/app/routes.py
+++ api/app/routes.py
@@ -1,7 +1,14 @@
 from app import app
-from flask import request
-import logging_system
+from flask import request, Blueprint
+from app import logging_system
 import json
+
+routes = Blueprint('routes', __name__)
+
+
+@app.route('/')
+def index():
+    return 'Index Page'
 
 
 @app.route('/hello_world')
@@ -16,4 +23,16 @@ def register():
 
     # Retrieve data from Front-End
     registration_form = request.get_data()
+
+    # Output data to Front-End
     return logging_system.registration(registration_form)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+
+    # Retrieve data from Front-End
+    login_form = request.get_data()
+
+    # Output data to Front-End
+    return logging_system.login(login_form)
