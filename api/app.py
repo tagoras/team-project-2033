6202c783a6da2ce3587ca04ca7c9a229cab4a0ad
Big Pineapple <91441379+IHatePineapples@users.noreply.github.com>
2021-12-20 06:09:07 +0000

    add jsonify wrapping to all responses

--- api/app.py
+++ api/app.py
@@ -24,8 +24,8 @@ def empty_values(dictionary):
 @app.route('/hello_world')
 def hello_world() -> json:
     # just for testing : return a hello world json object, for debugging api calls
-    return {'title': "Hello!",
-            'content': "Hello World"}
+    return jsonify({'title': "Hello!",
+                    'content': "Hello World"})
 
 
 @app.route('/register', methods=['GET', 'POST'])
@@ -35,40 +35,40 @@ def register() -> json:
         registration_form = request.json
         # Any empty values
         if empty_values(registration_form) == -1:
-            return {
+            return jsonify({
                 'status': -1,
                 'message': "Registration failed: Fill all fields"
-            }
+            })
 
         # Password Validating
         password_size = len(registration_form['password'])
         password_checker = re.compile(r'(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[*?!^+%&()=}{$#@<>])')
         if password_checker.match(registration_form['password']):
-            return {
+            return jsonify({
                 'status': -1,
                 'message': "Registration failed: Please check password"
-            }
+            })
         elif 6 > password_size > 12:
-            return {
+            return jsonify({
                 'status': -1,
                 'message': "Registration failed: Please check password"
-            }
+            })
 
         # Email Validating
         regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
         if not re.search(regex, registration_form['email']):
-            return {
+            return jsonify({
                 'status': -1,
                 'message': "Registration failed: Please check your email"
-            }
+            })
 
         # Postcode Validation
         regex = r'[A-Z]{1,2}[0-9R][0-9A-Z]? [0-9][A-Z]{2}'
         if not re.search(regex, registration_form['postcode']):
-            return {
+            return jsonify({
                 'status': -1,
                 'message': "Registration failed: Please check postcode"
-            }
+            })
 
         # One way encrypt
         registration_form['password'] = generate_password_hash(registration_form['password'])
@@ -77,16 +77,16 @@ def register() -> json:
         try:
             user = User.query.filter_by(email=registration_form['email']).first()
             if user:
-                return {
+                return jsonify({
                     'status': -1,
                     'message': "Registration failed: Email is already taken!"
-                }
+                })
         except Exception as e:
             print(e)
-            return {
+            return jsonify({
                 'status': -1,
                 'message': "Registration failed: Email might already be taken, try again later"
-            }
+            })
         try:
             new_user = User(username=registration_form['username'],
                             email=registration_form['email'],
@@ -94,21 +94,21 @@ def register() -> json:
                             password=registration_form['password'])
             db.session.add(new_user)
             db.session.commit()
-            return {
+            return jsonify({
                 'status': 0,
                 'message': "Account successfully registered"
-            }
+            })
         except Exception as e:
             print(e)
-            return {
+            return jsonify({
                 'status': -1,
                 'message': "Registration failed: Internal error"
-            }
+            })
     else:
-        return {
+        return jsonify({
             'status': -1,
             'message': "Registration failed: This is no json!!"
-        }
+        })
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -117,35 +117,35 @@ def login() -> json:
         login_form = request.json
 
         if empty_values(login_form) == -1:
-            return {
+            return jsonify({
                 'status': -1,
                 'message': "Login failed: Fill all fields"
-            }
+            })
 
         user = User.query.filter_by(username=login_form['username']).first()
 
         if not user or not check_password_hash(user.password, login_form['password']):
-            return {
+            return jsonify({
                 'status': -1,
                 'message': "Login failed: Username or password is incorrect!"
-            }
+            })
         try:
             login_user(user)
-            return {
+            return jsonify({
                 'status': 0,
                 'message': "User successfully logged in"
-            }
+            })
         except Exception as e:
             print(e)
-            return {
+            return jsonify({
                 'status': -1,
                 'message': "Login failed: Username or password might be incorrect. Please try again later"
-            }
+            })
     else:
-        return {
+        return jsonify({
             'status': -1,
             'message': "Login failed: This is no json!!"
-        }
+        })
 
 
 @app.route("/logout")
