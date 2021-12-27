4e7b02b72c569b506e9e052e9fdba270f89f1bd8
tagoras <pvaitkeviciuz@gmail.com>
2021-12-27 21:08:05 +0200

    Added Admin page

---
+++ src/Pages/Admin.js
@@ -0,0 +1,14 @@
+import React from "react";
+import {Navbar} from '../Components/Navbar/Navbar';
+import {ComplaintTable} from '../Components/ComplaintTable/ComplaintTable';
+
+function AdminPage(){
+
+
+
+    return(
+        <ComplaintTable/>
+    )
+}
+
+export default AdminPage;
