9d3d6734077e91b63f5cedec4f4613fce40014c2
tagoras <pvaitkeviciuz@gmail.com>
2021-12-27 21:09:13 +0200

    Added dummy data

---
+++ src/Components/ComplaintTable/ComplaintData.js
@@ -0,0 +1,148 @@
+export const data = [
+    {
+        id: 1,
+        subbmitBy: `John-1`,
+        complaintMessage: `#################`,
+        complaintPicture: ".jpg file here"
+    },
+    {
+        id: 2,
+        subbmitBy: `John-2`,
+        complaintMessage: `#################`,
+        complaintPicture: ".jpg file here"
+    },
+    {
+        id: 3,
+        subbmitBy: `John-3`,
+        complaintMessage: `#################`,
+        complaintPicture: ".jpg file here"
+    },
+    {
+        id: 4,
+        subbmitBy: `John-4`,
+        complaintMessage: `#################`,
+        complaintPicture: ".jpg file here"
+    },
+    {
+        id: 5,
+        subbmitBy: `John-5`,
+        complaintMessage: `#################`,
+        complaintPicture: ".jpg file here"
+    },
+    {
+        id: 6,
+        subbmitBy: `John-6`,
+        complaintMessage: `#################`,
+        complaintPicture: ".jpg file here"
+    },
+    {
+        id: 7,
+        subbmitBy: `John-7`,
+        complaintMessage: `#################`,
+        complaintPicture: ".jpg file here"
+    },
+    {
+        id: 8,
+        subbmitBy: `John-8`,
+        complaintMessage: `#################`,
+        complaintPicture: ".jpg file here"
+    },
+    {
+        id: 9,
+        subbmitBy: `John-9`,
+        complaintMessage: `#################`,
+        complaintPicture: ".jpg file here"
+    },
+    {
+        id: 10,
+        subbmitBy: `John-10`,
+        complaintMessage: `#################`,
+        complaintPicture: ".jpg file here"
+    },
+    {
+        id: 11,
+        subbmitBy: `John-11`,
+        complaintMessage: `#################`,
+        complaintPicture: ".jpg file here"
+    },
+    {
+        id: 12,
+        subbmitBy: `John-12`,
+        complaintMessage: `#################`,
+        complaintPicture: ".jpg file here"
+    },
+    {
+        id: 13,
+        subbmitBy: `John-13`,
+        complaintMessage: `#################`,
+        complaintPicture: ".jpg file here"
+    },
+    {
+        id: 14,
+        subbmitBy: `John-14`,
+        complaintMessage: `#################`,
+        complaintPicture: ".jpg file here"
+    },
+    {
+        id: 15,
+        subbmitBy: `John-15`,
+        complaintMessage: `#################`,
+        complaintPicture: ".jpg file here"
+    },
+    {
+        id: 16,
+        subbmitBy: `John-16`,
+        complaintMessage: `#################`,
+        complaintPicture: ".jpg file here"
+    },
+    {
+        id: 17,
+        subbmitBy: `John-17`,
+        complaintMessage: `#################`,
+        complaintPicture: ".jpg file here"
+    },
+    {
+        id: 18,
+        subbmitBy: `John-18`,
+        complaintMessage: `#################`,
+        complaintPicture: ".jpg file here"
+    },
+    {
+        id: 19,
+        subbmitBy: `John-19`,
+        complaintMessage: `#################`,
+        complaintPicture: ".jpg file here"
+    },
+    {
+        id: 20,
+        subbmitBy: `John-20`,
+        complaintMessage: `#################`,
+        complaintPicture: ".jpg file here"
+    },
+    {
+        id: 21,
+        subbmitBy: `John-21`,
+        complaintMessage: `#################`,
+        complaintPicture: ".jpg file here"
+    },
+    {
+        id: 22,
+        subbmitBy: `John-22`,
+        complaintMessage: `#################`,
+        complaintPicture: ".jpg file here"
+    },
+    {
+        id: 23,
+        subbmitBy: `John-23`,
+        complaintMessage: `#################`,
+        complaintPicture: ".jpg file here"
+    },
+    {
+        id: 24,
+        subbmitBy: `John-24`,
+        complaintMessage: `#################`,
+        complaintPicture: ".jpg file here"
+    },
+];
+
+export default data;
