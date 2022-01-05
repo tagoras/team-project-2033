41c9837746d76debc0d1fc7bbe3532be6756b11b
tagoras <pvaitkeviciuz@gmail.com>
2022-01-05 15:32:08 +0200

    Added admin page access request

--- src/GenericFunctions.js
+++ src/GenericFunctions.js
@@ -1,9 +1,11 @@
-export function sentSyncrhonousAccessRequest(page){
-    let result = await fetch(`/${page}`, {
+export async function sentSyncrhonousAccessRequest(page){
+    let result = await fetch(`${page}`, {
       method: "POST",
       headers: {
         'Content-Type': 'application/json;charset=utf-8'
       },
-      JWT: document.cookie.substring(10);
+      JWT: document.cookie.substring(10)
     });
+
+    return result.json();
 }
--- src/Pages/Admin.js
+++ src/Pages/Admin.js
@@ -2,8 +2,17 @@ import React from "react";
 import data from '../Components/Complaint/Data';
 import './Admin.style.css';
 import ComplaintCard from "../Components/Complaint/ComplaintCard";
+import "../GenericFunctions";
+import { sentSyncrhonousAccessRequest } from "../GenericFunctions";
 
 function AdminPage(){
+
+    let accessGranted = sentSyncrhonousAccessRequest("/admin");
+
+    accessGranted.then((resultInJSON => {console.log(resultInJSON)}, (Error) => console.log(Error)));
+
+    
+
     return(
         <div className="Container">
             <ul className="Grid-Unordered-List">
