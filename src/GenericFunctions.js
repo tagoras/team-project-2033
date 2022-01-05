9f41de349f8b74e2f903dccb4e875d539663362b
tagoras <pvaitkeviciuz@gmail.com>
2022-01-05 18:57:41 +0200

    changes in async request

--- src/GenericFunctions.js
+++ src/GenericFunctions.js
@@ -1,10 +1,14 @@
+import jwt, {Bearer} from "jsonwebtoken";
+
 export async function sentSyncrhonousAccessRequest(page){
+  console.log(page);
+  console.log(document.cookie.substring(10));
     let result = await fetch(`${page}`, {
       method: "POST",
       headers: {
-        'Content-Type': 'application/json;charset=utf-8'
+        'Content-Type': 'application/json;charset=utf-8',
+        'Authorization': Bearer `${document.cookie.substring(10)}`
       },
-      JWT: document.cookie.substring(10)
     });
 
     return result.json();
