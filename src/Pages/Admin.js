dfb87039e9bdc9a6490332a4e0bb35d6700e3a30
tagoras <pvaitkeviciuz@gmail.com>
2022-01-05 19:35:49 +0200

    Fixed unsuccessful async request

--- src/Pages/Admin.js
+++ src/Pages/Admin.js
@@ -4,14 +4,16 @@ import './Admin.style.css';
 import ComplaintCard from "../Components/Complaint/ComplaintCard";
 import "../GenericFunctions";
 import { sentSyncrhonousAccessRequest } from "../GenericFunctions";
+import {navigate} from 'react-router-dom';
 
 function AdminPage(){
 
     // Sent request for access by sending a cookie JWT
-    let accessGranted = sentSyncrhonousAccessRequest("/admin");
+    let accessGranted = sentSyncrhonousAccessRequest("/admin/view_all");
 
-    accessGranted.then((resultInJSON => {console.log(resultInJSON)}, (Error) => console.log(Error)));
-    // If access denied -> Render Error Page;
+    accessGranted.catch((responseInJSON) => console.log(responseInJSON));
+
+
     return(
         <div className="Container">
             <ul className="Grid-Unordered-List">
