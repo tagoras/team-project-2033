1ed7eee2de3c7c5419fc369111f5653f06a05338
tagoras <pvaitkeviciuz@gmail.com>
2022-01-05 15:10:47 +0200

    Added file to store prevalent functions

---
+++ src/GenericFunctions.js
@@ -0,0 +1,9 @@
+export function sentSyncrhonousAccessRequest(page){
+    let result = await fetch(`/${page}`, {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json;charset=utf-8'
+      },
+      JWT: document.cookie.substring(10);
+    });
+}
