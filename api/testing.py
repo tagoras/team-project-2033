b5083e89ea729ed1d590c48b4bea8d37c2173f14
Big Pineapple <lostinhollywood@duck.com>
2022-01-10 11:49:02 +0000

    Add /login to testing.py

--- api/testing.py
+++ api/testing.py
@@ -9,6 +9,12 @@ import app
 
 
 class FlaskApp(unittest.TestCase):
+    """
+        def __init__(self, JWT='', ADMIN_JWT=''):
+            super().__init__()
+            self.JWT = JWT
+            self.ADMIN_JWT = ADMIN_JWT
+    """
 
     def test_has_empty_value(self):
         d1 = {1: ""}
@@ -23,6 +29,7 @@ class FlaskApp(unittest.TestCase):
     def test_hello_world(self):
         r = requests.get('http://localhost:5000/hello_world')
         json_content = r.json()
+        print(json_content)
         self.assertEqual({'title': "Hello!", 'content': "Hello World"}, json_content[0])
         self.assertEqual('application/json', r.headers['Content-Type'], )
         self.assertEqual(200, r.status_code, )
@@ -58,6 +65,44 @@ class FlaskApp(unittest.TestCase):
                 'password': 'He110 w0r1d£'}
         r = requests.post(url=url, json=data)
         self.assertEqual(201, r.status_code)
+
+    def test_login(self):
+        url = 'http://localhost:5000/login'
+
+        login_data = {'username': 'An incorrect username',
+                      'password': 'He110 w0r1d£'}
+        r = requests.post(url=url, json=login_data)
+        self.assertEqual(406, r.status_code)
+
+        login_data = {'empty_fields_perhaps': ':(',
+                      'username': '',
+                      'password': ''}
+        r = requests.post(url=url, json=login_data)
+        self.assertEqual(406, r.status_code)
+
+        login_data = {'username': 'Test',
+                      'password': 'A not so good password :('}
+        r = requests.post(url=url, json=login_data)
+        self.assertEqual(406, r.status_code)
+
+        """        
+        login_data = {'username': 'Test',
+                      'password': 'He110 w0r1d£'}
+        r = requests.post(url=url, json=login_data)
+        self.assertEqual(202, r.status_code)
+
+        jwt = r.json()[0]['JWT']
+        print(jwt)
+        self.JWT = jwt
+        """
+        login_data = {'username': 'Joe',
+                      'password': 'Njdka3rq39h!'}
+        r = requests.post(url=url, json=login_data)
+        self.assertEqual(202, r.status_code)
+
+        admin_jwt = r.json()['JWT']
+        print(admin_jwt)
+        self.ADMIN_JWT = admin_jwt
 
 
 if __name__ == '__main__':
