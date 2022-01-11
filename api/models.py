4c62223a6de5d382fe48717ae6243d458e894ebc
c0025335 <c0025335@newcastle.ac.uk>
2022-01-11 10:37:03 +0000

    Added a test user and submission to use for testing purposes

--- api/models.py
+++ api/models.py
@@ -65,15 +65,32 @@ def init_db():
     db.drop_all()
     db.create_all()
 
-    test_user = User(username='Joe',
-                     email='test1@test.com',
-                     password=generate_password_hash('Njdka3rq39h!'),
-                     # postcode=generate_password_hash("NE6 9RU"),
-                     postcode="NE6 9RU",
-                     role='admin')
+    test_admin = User(username='Joe',
+                      email='test1@test.com',
+                      password=generate_password_hash('Njdka3rq39h!'),
+                      # postcode=generate_password_hash("NE6 9RU"),
+                      postcode="NE6 9RU",
+                      role='admin')
 
+    test_user = User(username='Steve',
+                     email='Steve@test.com',
+                     password=generate_password_hash('Pass123'),
+                     # postcode=generate_password_hash("NE6 9RU"),
+                     postcode="NE1 2AA",
+                     role='user')
+
+    test_submission = Complaint(user_id=2,
+                                title='Test Submission',
+                                description='To use for testing',
+                                postcode='NE8 8BC',
+                                date='1/5/2022',
+                                img_path='data/cats/cat.jpg')
+
+    db.session.add(test_admin)
     db.session.add(test_user)
+    db.session.add(test_submission)
     db.session.commit()
+
     import requests
     import shutil
     os.system("mkdir data/cats")
